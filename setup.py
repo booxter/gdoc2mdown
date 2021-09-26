2e003426eb7d063387bf67f622fb04a207ea8c81
Ihar Hrachyshka <ihrachys@redhat.com>
2021-09-26 13:37:30 -0400

    Drop <python@3.9 support

--- setup.py
+++ setup.py
@@ -21,16 +21,13 @@ test_requirements = [ ]
 setup(
     author="Ihar Hrachyshka",
     author_email='hrachyshka@gmail.com',
-    python_requires='>=3.6',
+    python_requires='>=3.9',
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
     ],
     description="Python module to import from Google Docs to Markdown",
