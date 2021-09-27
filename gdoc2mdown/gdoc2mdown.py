c0fcfe1d4ae47021ae9b6dad0e70d890d0b0ed5d
Ihar Hrachyshka <ihrachys@redhat.com>
2021-09-27 17:02:33 -0400

    Split out code working with gdoc and markdown

--- gdoc2mdown/gdoc2mdown.py
+++ gdoc2mdown/gdoc2mdown.py
@@ -1,1 +1,36 @@
-"""Main module."""
+def read_paragraph_element(element):
+    """Returns the text in the given ParagraphElement.
+        Args:
+            element: a ParagraphElement from a Google Doc.
+    """
+    text_run = element.get('textRun')
+    if not text_run:
+        return ''
+    return text_run.get('content')
+
+
+def read_strucutural_elements(elements):
+    """Recurses through a list of Structural Elements to read a document's text where text may be
+        in nested elements.
+        Args:
+            elements: a list of Structural Elements.
+    """
+    text = ''
+    for value in elements:
+        if 'paragraph' in value:
+            elements = value.get('paragraph').get('elements')
+            for elem in elements:
+                text += read_paragraph_element(elem)
+        elif 'table' in value:
+            # The text in table cells are in nested Structural Elements and tables may be
+            # nested.
+            table = value.get('table')
+            for row in table.get('tableRows'):
+                cells = row.get('tableCells')
+                for cell in cells:
+                    text += read_strucutural_elements(cell.get('content'))
+        elif 'tableOfContents' in value:
+            # The text in the TOC is also in a Structural Element.
+            toc = value.get('tableOfContents')
+            text += read_strucutural_elements(toc.get('content'))
+    return text
